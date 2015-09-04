e42c514e90e23b5f22b696828935bee687f56b1e
fphg <fabrice@phung.fr>
2015-09-04 15:51:11 +0200

    fix locations and missing resources

--- mdchecker/__init__.py
+++ mdchecker/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-app.config.from_pyfile('conf/server.cfg', silent=False)
+app.config.from_pyfile('conf/server.cfg', silent=True)
 
 import mdchecker.main
