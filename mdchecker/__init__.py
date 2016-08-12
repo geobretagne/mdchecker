3e2facca6e43687d8ec535535b264522ebc49d9b
bchartier <benjamin.chartier@cegetel.net>
2016-08-12 09:56:39 +0200

    Add templates

--- mdchecker/__init__.py
+++ mdchecker/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('conf/server.cfg', silent=True)
+app.jinja_env.add_extension('jinja2.ext.do')
 db = SQLAlchemy(app)
 
 import mdchecker.main
