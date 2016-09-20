cc8e6cf0a7955576e8bd6cf46fd258bbe845340b
bchartier <benjamin.chartier@cegetel.net>
2016-09-20 10:35:38 +0200

    Add try/except in create_db.py and improve logs

--- app/create_db.py
+++ app/create_db.py
@@ -1,9 +1,9 @@
 import os
 import sys
 import logging
+import sqlalchemy.exc
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('mdchecker')
 
 sys.path.append(os.getcwd())
 from mdchecker.main import db
@@ -11,9 +11,18 @@ from mdchecker.main import db
 
 if __name__ == '__main__':
 
-    logger.info(u"Creation of the database: {0}".format(db.app and db.app.config['SQLALCHEMY_DATABASE_URI'] or None))
-    db.create_all()
-    if not db.engine.has_table('test_session'):
-        logger.error(u"Database creation failed")
-    else:
-        logger.info(u"Database creation succeeded")
+    logging.info(u"Creation of the database: {0}".format(db.app and db.app.config['SQLALCHEMY_DATABASE_URI'] or None))
+
+    try:
+        db.create_all()
+
+        if db.engine.has_table('test_session'):
+            logging.info(u"Database creation succeeded")
+
+    except sqlalchemy.exc.OperationalError as e:
+        msg = u"Database creation failed - {0} - {1}".format(
+            type(e),
+            e.message
+        )
+        logging.error(msg)
+        logging.info(u"Check the path of the database file that should have been created")
